26d53976f6bf2f7937018371c75ae60edf0c3190
Samuel Fortunato <samfortunato@gmail.com>
2021-10-16 15:22:01 -0400

    Make basic under construction page

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.scss';
+
+export function Header() {
+  return (
+    <header>
+      <h1>superatomic</h1>
+    </header>
+  );
+}
---
+++ src/components/launch-date/launch-date.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './launch-date.scss';
+
+export function LaunchDate() {
+  return <span>Coming soon...</span>;
+}
